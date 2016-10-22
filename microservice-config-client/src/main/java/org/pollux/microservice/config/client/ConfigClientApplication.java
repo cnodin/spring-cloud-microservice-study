4f35366c5fb12de8177c104ee37c9b02ce282f88
spockwwang(汪彬彬) <spockwwang@tencent.com>
2016-10-22 19:44:28 +0800

    update microservice-config-client.properties

---
+++ microservice-config-client/src/main/java/org/pollux/microservice/config/client/ConfigClientApplication.java
@@ -0,0 +1,7 @@
+package org.pollux.microservice.config.client;
+
+/**
+ * Created by spockwwang on 2016/10/22.
+ */
+public class ConfigClientApplication {
+}
---
+++ microservice-config-client/src/main/java/org/pollux/microservice/config/client/controller/ConfigClientController.java
@@ -0,0 +1,7 @@
+package org.pollux.microservice.config.client.controller;
+
+/**
+ * Created by spockwwang on 2016/10/22.
+ */
+public class ConfigClientController {
+}
