f1c43df963c5d179b3f2133f43cec197a9c544ae
spockwwang(汪彬彬) <spockwwang@tencent.com>
2016-10-23 11:42:43 +0800

    update microservice-config-client-dev.properties

---
+++ microservice-config-client-eureka/src/main/java/org/pollux/microservice/config/client/eureka/ConfigClientEurekaApplication.java
@@ -0,0 +1,7 @@
+package org.pollux.microservice.config.client.eureka;
+
+/**
+ * Created by spockwwang on 2016/10/23.
+ */
+public class ConfigClientEurekaApplication {
+}
---
+++ microservice-config-server-eureka/src/main/java/org/pollux/microservice/config/server/eureka/ConfigServerEurekaApplication.java
@@ -0,0 +1,7 @@
+package org.pollux.microservice.config.server.eureka;
+
+/**
+ * Created by spockwwang on 2016/10/23.
+ */
+public class ConfigServerEurekaApplication {
+}
